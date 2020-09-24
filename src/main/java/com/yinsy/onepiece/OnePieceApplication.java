15bde21ac8a85dfeb4a62b8415af3ee250da00ae
yinsongyuan <1074754214@qq.com>
2020-09-24 11:00:13 +0800

    Zaanse Schans - 项目初始化

---
+++ src/main/java/com/yinsy/onepiece/OnePieceApplication.java
@@ -0,0 +1,14 @@
+package com.yinsy.onepiece;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePieceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnePieceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yinsy/onepiece/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.yinsy.onepiece.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("demo")
+public class DemoController {
+
+    @GetMapping("first")
+    public void firstDemo(String param){
+        System.out.println(param);
+    }
+}
